f20caf82576ea89c0c69aec94e9b7e58f2dacc37
j178 <10510431+j178@users.noreply.github.com>
2020-08-06 16:49:09 +0800

    Create some models

--- v2ex/models.py
+++ v2ex/models.py
@@ -2,6 +2,59 @@
 # Created by johnj at 2020/8/6
 import enum
 from dataclasses import dataclass
+from datetime import datetime
+from typing import AsyncGenerator
+
+
+@dataclass
+class Node:
+    name: str
+    topics_num: int
+    description: str
+
+    async def topics(self) -> AsyncGenerator:
+        pass
+
+
+@dataclass
+class Member:
+    id: str
+    number: str
+    join_time: datetime
+    avatar_link: str
+
+    async def topics(self) -> AsyncGenerator['Topic']:
+        pass
+
+    async def replies(self) -> AsyncGenerator['Reply']:
+        pass
+
+    async def notifications(self) -> AsyncGenerator['Notification']:
+        pass
+
+
+class Me(Member):
+
+    async def create_topic(self, title: str, content: str, node: 'Node') -> 'Topic':
+        pass
+
+
+@dataclass
+class Reply:
+    author: Member
+    create_time: datetime
+    topic: 'Topic'
+    content: str
+
+
+@dataclass
+class Topic:
+    id: int
+    author: Member
+    create_time: datetime
+
+    async def replies(self) -> AsyncGenerator['Reply']:
+        pass
 
 
 class NotifyType(enum.Enum):
@@ -12,7 +65,7 @@ class NotifyType(enum.Enum):
     OTHER = 'other'
 
 
-@dataclass()
+@dataclass
 class Notification:
     id: int
     time: str
